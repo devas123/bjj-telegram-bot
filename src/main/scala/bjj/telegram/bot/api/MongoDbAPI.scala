ee23a836732e844e7a22a03d2cc059ea060e6475
ggrigori <grigoriy.grigoriev@t-systems.com>
2019-11-09 03:33:56 +0300

    fixed a bug

--- src/main/scala/bjj/telegram/bot/api/MongoDbAPI.scala
+++ src/main/scala/bjj/telegram/bot/api/MongoDbAPI.scala
@@ -3,7 +3,7 @@ package bjj.telegram.bot.api
 import bjj.telegram.bot.di.ConfigModule
 import bjj.telegram.bot.model.User
 import bjj.telegram.bot.model.domain.ClassDescription
-import org.mongodb.scala.model.{Filters, Updates}
+import org.mongodb.scala.model.{Filters, Updates, Indexes}
 import org.mongodb.scala.result.UpdateResult
 
 class MongoDbAPI extends ConfigModule {
@@ -12,7 +12,6 @@ class MongoDbAPI extends ConfigModule {
   import org.mongodb.scala._
   import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
   import org.mongodb.scala.bson.codecs.Macros._
-  import org.mongodb.scala.model.Indexes._
 
   private val codecRegistry = fromRegistries(fromProviders(classOf[ClassDescription]), DEFAULT_CODEC_REGISTRY, fromProviders(classOf[User]))
   private val uri: String = config.getString("mongodb.url")
@@ -20,7 +19,7 @@ class MongoDbAPI extends ConfigModule {
   private val client: MongoClient = MongoClient(uri)
   private val db: MongoDatabase = client.getDatabase("telegrambot").withCodecRegistry(codecRegistry)
   private val collection: MongoCollection[ClassDescription] = db.getCollection(classCollectionName)
-  collection.createIndex(descending("creationTime"))
+  collection.createIndex(Indexes.descending("creationTime"))
 
   def saveClassDescritpion(classDescription: ClassDescription): SingleObservable[Completed] = {
     collection.insertOne(classDescription)
@@ -35,7 +34,8 @@ class MongoDbAPI extends ConfigModule {
   }
 
   def getLatestClassDescription(channelId: String): SingleObservable[ClassDescription] = {
-    collection.find(Filters.eq("channelId", channelId)).first()
+    import org.mongodb.scala.model.Sorts._
+    collection.find(Filters.eq("channelId", channelId)).sort(descending("creationTime", "_id")).first()
   }
 
   def removeAttendeeFromTheLatest(user: User, channelId: String): Observable[UpdateResult] = {
